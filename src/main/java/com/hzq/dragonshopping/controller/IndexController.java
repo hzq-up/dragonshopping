3088897c75e8e3447562e0fd2204d147f952b30e
MusciMan <hzq_up@126.com>
2019-10-30 12:30:17 +0800

    添加日志

--- src/main/java/com/hzq/dragonshopping/controller/IndexController.java
+++ src/main/java/com/hzq/dragonshopping/controller/IndexController.java
@@ -2,6 +2,8 @@ package com.hzq.dragonshopping.controller;
 
 import com.hzq.dragonshopping.service.IProduceCategoryService;
 import com.hzq.dragonshopping.service.IProduceService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
+
+    private final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
     @Autowired
     private IProduceService produceService;
@@ -28,6 +32,7 @@ public class IndexController {
         Model produce = model.addAttribute("produceList", produceService.showHotCommody());
         //所有商品的分类信息
         model.addAttribute("produceCategory",iProduceCategoryService.selectProduceCategoryExample());
+        logger.info("数据");
         return "index";
     }
 
